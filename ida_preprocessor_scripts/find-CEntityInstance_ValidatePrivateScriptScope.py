9f4e9580d76897c5aac298045b31313efdf010a9
hzqst <113660872@qq.com>
2026-05-11 15:18:23 +0800

    Add find-CEntityInstance_ValidatePrivateScriptScope preprocessor script

---
+++ ida_preprocessor_scripts/find-CEntityInstance_ValidatePrivateScriptScope.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""Preprocess script for find-CEntityInstance_ValidatePrivateScriptScope skill."""
+
+from ida_analyze_util import preprocess_common_skill
+
+TARGET_FUNCTION_NAMES = [
+    "CEntityInstance_ValidatePrivateScriptScope",
+]
+
+FUNC_XREFS = [
+    {
+        "func_name": "CEntityInstance_ValidatePrivateScriptScope",
+        "xref_strings": [
+            "thisEntity",
+        ],
+        "xref_gvs": [], "xref_signatures": [], "xref_funcs": [],
+        "exclude_funcs": [], "exclude_strings": ["InputRunPrivateScript"], "exclude_gvs": [], "exclude_signatures": [],
+    },
+]
+
+GENERATE_YAML_DESIRED_FIELDS = [
+    (
+        "CEntityInstance_ValidatePrivateScriptScope",
+        [
+            "func_name",
+            "func_sig",
+            "func_va",
+            "func_rva",
+            "func_size",
+        ],
+    ),
+]
+
+async def preprocess_skill(
+    session, skill_name, expected_outputs, old_yaml_map,
+    new_binary_dir, platform, image_base, debug=False,
+):
+    """Reuse previous gamever func_sig to locate target function(s) and write YAML."""
+    return await preprocess_common_skill(
+        session=session,
+        expected_outputs=expected_outputs,
+        old_yaml_map=old_yaml_map,
+        new_binary_dir=new_binary_dir,
+        platform=platform,
+        image_base=image_base,
+        func_names=TARGET_FUNCTION_NAMES,
+        func_xrefs=FUNC_XREFS,
+        generate_yaml_desired_fields=GENERATE_YAML_DESIRED_FIELDS,
+        debug=debug,
+    )
