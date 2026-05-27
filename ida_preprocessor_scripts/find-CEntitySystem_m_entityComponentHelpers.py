57bc4a687a230503ad6c90518d30955561fc9ee0
Jerry.J.Jiang <jerry.j.jiang@benq.com>
2026-05-27 12:19:50 +0800

    Add find-CEntitySystem_m_entityComponentHelpers (Pattern E)

---
+++ ida_preprocessor_scripts/find-CEntitySystem_m_entityComponentHelpers.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+"""Preprocess script for find-CEntitySystem_m_entityComponentHelpers skill."""
+
+from ida_analyze_util import preprocess_common_skill
+
+TARGET_STRUCT_MEMBER_NAMES = [
+    "CEntitySystem_m_entityComponentHelpers",
+]
+
+LLM_DECOMPILE = [
+    # (symbol_name, path_to_prompt, path_to_reference)
+    (
+        "CEntitySystem_m_entityComponentHelpers",
+        "prompt/call_llm_decompile.md",
+        "references/server/CEntitySystem_FindComponentTypeByName.{platform}.yaml",
+    ),
+]
+
+GENERATE_YAML_DESIRED_FIELDS = [
+    (
+        "CEntitySystem_m_entityComponentHelpers",
+        [
+            "struct_name",
+            "member_name",
+            "offset",
+            "offset_sig",
+            "offset_sig_disp",
+        ],
+    ),
+]
+
+
+async def preprocess_skill(
+    session, skill_name, expected_outputs, old_yaml_map,
+    new_binary_dir, platform, image_base, llm_config=None, debug=False,
+):
+    """Reuse previous gamever offset_sig to locate target struct offset and write YAML."""
+    return await preprocess_common_skill(
+        session=session,
+        expected_outputs=expected_outputs,
+        old_yaml_map=old_yaml_map,
+        new_binary_dir=new_binary_dir,
+        platform=platform,
+        image_base=image_base,
+        struct_member_names=TARGET_STRUCT_MEMBER_NAMES,
+        llm_decompile_specs=LLM_DECOMPILE,
+        llm_config=llm_config,
+        generate_yaml_desired_fields=GENERATE_YAML_DESIRED_FIELDS,
+        debug=debug,
+    )
